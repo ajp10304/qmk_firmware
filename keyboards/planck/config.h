8c2c3fb18f5d69026713e7df9fbbd088912d238d
Alan Pocklington <a.pocklington@gmail.com>
2020-09-09 19:51:55 +0100

    TVR Planck pins

--- keyboards/planck/config.h
+++ keyboards/planck/config.h
@@ -32,8 +32,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MATRIX_COLS 12
 
 /* Planck PCB default pin-out */
-#define MATRIX_ROW_PINS { D0, D5, B5, B6 }
-#define MATRIX_COL_PINS { F1, F0, B0, C7, F4, F5, F6, F7, D4, D6, B4, D7 }
+#define MATRIX_ROW_PINS { F7, D4, D2, D1 }
+#define MATRIX_COL_PINS { B1, B0, F0, D6, B2, B3, B7, D0, D5, C7, D3, C6 }
 #define UNUSED_PINS
 
 #define QMK_ESC_OUTPUT F1
